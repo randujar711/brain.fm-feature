8ce72e396a94d6478c92c477dd4c9332b9a61b7d
Ryan Andujar <randujar711@gmail.com>
2023-06-28 14:00:28 -0400

    quick update

--- Comp/State.tsx
+++ Comp/State.tsx
@@ -5,11 +5,13 @@ import { View, Text, StyleSheet, ScrollView, Image } from
 type MentalStateProps = {
   MentalState: {
     state: string;
-    image: string;
+    image: number;
   }[];
 }
 
 export const State = (props: MentalStateProps) => {
+    const getCardStyle = (state: string) =>
+      state === 'meditate.' ? StyleSheet.flatten([styles.card, styles.mediCard]) : styles.card;
 
   return (
     <ScrollView 
@@ -18,12 +20,14 @@ export const State = (props: MentalStateProps) => {
       showsVerticalScrollIndicator={false}
     >
       {
-        props.MentalState.map((e) => {
+        props.MentalState.map((e, i) => {
+          // console.log(e)
+          const marginLeftStyle = i === 3 ? null : { marginLeft: 50 };
             return(
-                <View style={styles.card}> 
+                <View style={ getCardStyle(e.state)}> 
                     <Text style={styles.text}>{e.state}</Text>
                     <Image 
-                      style={{width: '40%', height: '95%', borderRadius: 16}}
+                      style={[{width: '40%', height: '95%', borderRadius: 16}, marginLeftStyle]}
                       source={e.image}
                     />
                 </View>
@@ -45,6 +49,9 @@ const styles = StyleSheet.create({
     text: {
         color: '#fff',
         fontSize: 40,
+    },
+    mediCard:{
+      width: '80%'
     },
     card: {
         flexDirection: 'row',
